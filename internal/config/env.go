58386564ebbe6df961fc7fbdc123b64a61901846
Arthur-Conti <arthurconti2003@gmail.com>
2024-09-03 14:57:29 -0300

    First commit

---
+++ internal/config/env.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+func GetEnv(envName string) string {
+	envValue, envExists := os.LookupEnv(envName)
+	if !envExists {
+		log.Fatalf("error getting env: %s", envName)
+	}
+	return envValue
+}
